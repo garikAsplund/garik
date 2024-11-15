b1e24c02cf576ae7c7d487e540f2b98e47b8f063
Garik Asplund <garik.asplund@gmail.com>
2024-11-15 13:45:21 -0800

    cleaning up animation

---
+++ src/lib/HeroArt.svelte
@@ -0,0 +1,87 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import random from 'canvas-sketch-util/random';
+	import { mapRange } from 'canvas-sketch-util/math';
+
+	const { noise2D } = random;
+
+	let canvas = $state();
+    let enableAnimation = $state(false);
+
+
+	onMount(() => {
+        const isLargeScreen = window.innerWidth > 768;
+		const prefersReducedMotion = window.matchMedia('(prefers-reduced-motion: reduce)').matches;
+
+		if (!isLargeScreen || prefersReducedMotion) return;
+
+		enableAnimation = true; 
+        
+		const ctx = canvas.getContext('2d')!;
+		const width = (canvas.width = window.innerWidth);
+		const height = (canvas.height = window.innerHeight);
+
+		// Grid settings
+		const cols = 99;
+		const rows = 200;
+		const numCells = cols * rows;
+
+		// Grid dimensions
+		const gw = width * 0.8;
+		const gh = height * 0.8;
+		const cw = gw / cols;
+		const ch = gh / rows;
+
+		// Margins
+		const mx = (width - gw) * 0.5;
+		const my = (height - gh) * 0.5;
+
+		// Noise and color settings
+		const frequency = 0.003;
+		const amplitude = 39;
+
+		// Points array
+		const points = Array.from({ length: numCells }, (_, i) => {
+			const x = (i % cols) * cw + mx;
+			const y = Math.floor(i / cols) * ch + my;
+			const n = noise2D(x, y, frequency, amplitude);
+			const lineWidth = mapRange(n, -amplitude, amplitude, 22, 7);
+			const color = `hsl(${mapRange(n, -amplitude, amplitude, 333, 250)}, 70%, 95%)`;
+
+			return { x, y, ix: x, iy: y, lineWidth, color };
+		});
+
+		let frame = 0;
+		const animate = () => {
+            if (!enableAnimation) return;
+
+			ctx.clearRect(0, 0, width, height);
+
+			// Update grid points
+			for (let i = 0; i < points.length; i++) {
+				const point = points[i];
+				const n = noise2D(point.ix + frame * 2, point.iy, frequency, amplitude);
+				point.x = point.ix + n;
+				point.y = point.iy + n;
+			}
+
+			ctx.lineWidth = 1;
+
+			// Draw grid points
+			for (let i = 0; i < points.length; i++) {
+				const point = points[i];
+				ctx.beginPath();
+				ctx.arc(point.x, point.y, point.lineWidth * 0.2, 0, Math.PI * 2);
+				ctx.fillStyle = point.color;
+				ctx.fill();
+			}
+
+			frame += 0.2;
+			requestAnimationFrame(animate);
+		};
+
+		animate();
+	});
+</script>
+
+<canvas bind:this={canvas} class="absolute inset-0 z-10 h-[70vh] w-full"></canvas>
--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -4,7 +4,7 @@
 	import { onMount } from 'svelte';
 	import { ChevronDown, X } from 'lucide-svelte';
 	import NavItems from '$lib/NavItems.svelte';
-	import {isMenuOpen} from '$lib/isMenuOpen.svelte';
+	import { isMenuOpen } from '$lib/isMenuOpen.svelte';
 
 	let lastScrollY = $state(0);
 	let isNavbarVisible = $state(true);
@@ -59,28 +59,28 @@
 <svelte:window on:scroll={handleScroll} />
 
 <header
-	class="sticky top-0 z-20 flex w-full {isNavbarVisible
+	class="sticky top-0 z-30 flex w-full {isNavbarVisible
 		? 'translate-y-0'
-		: '-translate-y-full'} md:py-4 text-gray-800 {isNavigating
+		: '-translate-y-full'} text-gray-800 md:py-4 {isNavigating
 		? 'transition-none'
-		: 'transition-transform duration-1000 ease-in-out'} items-center md:justify-around bg-slate-100 px-8"
+		: 'transition-transform duration-1000 ease-in-out'} items-center bg-slate-100 px-8 md:justify-around"
 	id="nav"
 	aria-label="Main navigation"
 >
 	<!-- Home link and logo -->
 	<div class="">
-		<a href="/" class="hover:opacity-70" aria-label="Home">
+		<a href="/" class="hover:opacity-70" aria-label="Home" onclick={() => isMenuOpen.button = false}>
 			<GarikCodes />
 		</a>
 	</div>
 
 	<!-- Nav bar -->
-	<nav class="md:flex w-full justify-end hidden">
+	<nav class="hidden w-full justify-end md:flex">
 		<NavItems />
 	</nav>
 
 	<!-- Mobile nav -->
-	<div class="flex justify-end space-x-2 md:hidden w-full">
+	<div class="flex w-full justify-end space-x-2 md:hidden">
 		<button
 			aria-label={isMenuOpen.button ? 'Close menu' : 'Open menu'}
 			aria-expanded={isMenuOpen.button}
@@ -99,14 +99,14 @@
 
 	<!-- Mobile Dropdown Menu -->
 	{#if isMenuOpen.button}
-	<div
-			class="absolute left-0 top-12 z-10 flex w-full flex-col items-center space-y-4 bg-slate-100 py-6 text-gray-800 shadow-lg md:hidden "
+		<div
+			class="absolute left-0 top-12 z-30 flex w-full flex-col items-center space-y-4 bg-slate-100 py-6 text-gray-800 shadow-lg md:hidden"
 			id="mobile-menu"
 			role="navigation"
 			aria-label="Mobile menu"
 		>
-		<NavItems />
-	</div>
+			<NavItems />
+		</div>
 	{/if}
 	<!-- CTA -->
 </header>
