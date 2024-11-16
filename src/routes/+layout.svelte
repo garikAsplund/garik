a5ba4d96229bc5cc3b0f2698c43e6dfe13717b0e
Garik Asplund <garik.asplund@gmail.com>
2024-11-15 16:09:46 -0800

    title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,11 +4,17 @@
 	import '../app.css';
 	import { dev } from '$app/environment';
 	import { injectAnalytics } from '@vercel/analytics/sveltekit';
+	import { page } from '$app/stores';
+	import { replaceState } from '$app/navigation';
 
 	injectAnalytics({ mode: dev ? 'development' : 'production' });
 
 	let { children } = $props();
 </script>
+
+<svelte:head>
+	<title>GAR1K.C0DES {$page.url.pathname === '/' ? '' : '| '} {($page.url.pathname.replace('/', '').charAt(0).toUpperCase()) + $page.url.pathname.slice(2).split('/')[0]}</title>
+</svelte:head>
 
 <div class="flex min-h-screen w-full flex-col bg-slate-100 dark:invert">
 	<Nav />
