e2cdcb5f7fd314679757a6c0d4d16cd45f5d4791
Garik Asplund <garik.asplund@gmail.com>
2024-11-17 16:51:19 -0800

    adding blog og images finally

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,31 +6,6 @@
 	let { data } = $props();
 </script>
 
-<svelte:head>
-  <title>GAR1K.C0DES</title>
-  <meta name="description" content="Independent software engineer for hire." />
-  
-  <!-- Facebook Meta Tags -->
-  <meta property="og:url" content="https://garik.codes" />
-  <meta property="og:type" content="website" />
-  <meta property="og:title" content="Garik Asplund | Software Engineer" />
-  <meta property="og:description" content="Need a fresh website? I've got you :)" />
-  <meta
-    property="og:image"
-    content="https://media.licdn.com/dms/image/v2/D5616AQFDLZGPXiZ-QA/profile-displaybackgroundimage-shrink_350_1400/profile-displaybackgroundimage-shrink_350_1400/0/1687973966708?e=1736985600&v=beta&t=IOnuEVPLdtSOSenBSgW6z9ZkbtBcj5Bbj3k9xDz3DJY"
-  />
-  
-  <!-- Twitter Meta Tags -->
-  <meta name="twitter:card" content="summary_large_image" />
-  <meta property="twitter:url" content="https://garik.codes" />
-  <meta name="twitter:title" content="Garik Asplund | Software Engineer" />
-  <meta name="twitter:description" content="Need a fresh website? I've got you :)" />
-  <meta
-    name="twitter:image"
-    content="https://media.licdn.com/dms/image/v2/D5616AQFDLZGPXiZ-QA/profile-displaybackgroundimage-shrink_350_1400/profile-displaybackgroundimage-shrink_350_1400/0/1687973966708?e=1736985600&v=beta&t=IOnuEVPLdtSOSenBSgW6z9ZkbtBcj5Bbj3k9xDz3DJY"
-  />
-</svelte:head>
-
 <div class="relative flex min-h-screen w-full flex-col items-center overflow-hidden">
 	<Blob />
 	<div class="relative z-20">
