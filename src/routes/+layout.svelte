835d908397d0d8ed9bd9ba227a91051b12e8d580
Garik Asplund <garik.asplund@gmail.com>
2024-11-20 12:41:16 -0800

    added speedinsights

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,13 +4,15 @@
 	import '../app.css';
 	import { dev } from '$app/environment';
 	import { injectAnalytics } from '@vercel/analytics/sveltekit';
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 
+	injectSpeedInsights();
 	injectAnalytics({ mode: dev ? 'development' : 'production' });
 
 	let { children } = $props();
 </script>
 
-<div class="flex min-h-screen w-full flex-col bg-slate-100 dark:invert text-gray-800">
+<div class="flex min-h-screen w-full flex-col bg-slate-100 text-gray-800 dark:invert">
 	<Nav />
 	<main class=" flex flex-col items-center">
 		{@render children()}
