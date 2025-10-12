27c1affb5a0369579e5db157867fe264e7f002fa
Garik Asplund <garik.asplund@gmail.com>
2025-10-12 13:13:12 -0700

    added resend and hello@ email for professionalism

--- src/routes/api/send-email/+server.ts
+++ src/routes/api/send-email/+server.ts
@@ -3,20 +3,20 @@ import nodemailer from 'nodemailer';
 export async function POST({ request }) {
 	const formData = await request.json();
 	const transporter = nodemailer.createTransport({
-		host: 'smtp.gmail.com',
-		port: 587,
-		secure: false,
+		host: 'smtp.resend.com',
+		port: 465,
+		secure: true,
 		auth: {
-			user: import.meta.env.VITE_EMAIL_USER,
-			pass: import.meta.env.VITE_EMAIL_PASSWORD
+			user: process.env.RESEND_USER,
+			pass: process.env.RESEND_PASSWORD,
 		}
 	});
 
-	const { name, email, phone, inquiry } = formData.data;
+	const { name, email, inquiry } = formData.data;
 
 	// Email to me
 	const businessMailOptions = {
-		from: 'garik.asplund@gmail.com',
+		from: `"Garik Asplund — Garik Codes" <hello@garik.codes>`,
 		to: 'garik.asplund@gmail.com',
 		subject: 'New Garik Codes Inquiry',
 		html: `
@@ -24,15 +24,14 @@ export async function POST({ request }) {
             <div style="background-color: #f9f9f9; padding: 1rem; border-radius: 5px;">
             <p><strong>Name:</strong> ${name}</p>
             <p><strong>Email:</strong> ${email}</p>
-            <p><strong>Phone:</strong> ${phone}</p>
-            <p><strong>Additional Comments:</strong> ${inquiry}</p>
+            <p><strong>Message:</strong> ${inquiry}</p>
             </div>
         `
 	};
 
 	// Email to the customer
 	const customerMailOptions = {
-		from: 'garik.asplund@gmail.com',
+		from: `"Garik Asplund — Garik Codes" <hello@garik.codes>`,
 		to: email, // email
 		subject: 'Your Garik Codes Inquiry',
 		html: `
