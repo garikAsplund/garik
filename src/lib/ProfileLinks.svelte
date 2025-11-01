4955293932e4da5559b5731325c8ec1558d272c6
Garik Asplund <garik.asplund@gmail.com>
2025-11-01 13:08:50 -0700

    added bluesky icon and link

--- src/lib/ProfileLinks.svelte
+++ src/lib/ProfileLinks.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
 	import GitHub from '$lib/SVGs/GitHub.svelte';
 	import LinkedIn from '$lib/SVGs/LinkedIn.svelte';
+	import Bluesky from './SVGs/Bluesky.svelte';
 </script>
-<div class="flex flex-col md:flex-row px-6 gap-6">
+<div class="flex flex-col md:flex-row px-6 gap-6 place-items-center">
 <a
 		href="https://github.com/garikAsplund"
 		target="_blank"
@@ -20,5 +21,14 @@
 		class="hover:opacity-80"
 	>
 		<LinkedIn />
+	</a>
+	<a
+		href="https://bsky.app/profile/garik.codes"
+		target="_blank"
+		rel="noopener noreferrer"
+		aria-label="Bluesky Profile"
+		class="hover:opacity-80"
+	>
+		<Bluesky />
 	</a>
 </div>
